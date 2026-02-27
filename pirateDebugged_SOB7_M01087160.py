456ddf995e65cc33f3797a8f6de7a9d7221f11d9
IqbalFirmansyah04 <if179@live.mdx.ac.uk>
2026-02-27 15:57:12 +0400

    Delete pirateDebugged_SOB7_M01087160.py

--- pirateDebugged_SOB7_M01087160.py
+++
@@ -1,5 +0,0 @@
-greeting = input("Hello, possible pirate! What's the password?") #added quotation
-if greeting == "Arrr!": #added proper syntax
-    print("Go away, pirate.")
-else: #replace elif with else
-    print("Greetings, hater of pirates!")
