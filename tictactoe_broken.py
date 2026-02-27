5e9bf485b8f5f7148c0bb070a5527d294158ae5e
IqbalFirmansyah04 <if179@live.mdx.ac.uk>
2026-02-27 16:05:20 +0400

    broken_tictactoe_debugged.py

--- tictactoe_broken.py
+++ tictactoe_broken.py
@@ -67,7 +67,7 @@ def add_piece(game, player, row, column):
     row: 0-index row
     column: 0-index column
     """
-    game[row][column+1] = player
+    game[row][column] = player #1; the piece should be directly added to the specified row or column, instead of adding +1 to it.
     return game
 
 def check_space_empty(game, row, column):
@@ -77,7 +77,7 @@ def convert_input_to_coordinate(user_input):
     return user_input - 1
 
 def switch_player(player):
-    if player = 1:
+    if player == 1: #== instead of =
         return 2
     else:
         return 1
@@ -98,12 +98,12 @@ if __name__ == '__main__':
     while winner == 0 and moves_exist(game):
         print("Currently player: " + str(player))
         available = False
-        while not available
+        while not available: #2; add : after available
             row = convert_input_to_coordinate(int(input("Which row? (start with 1) ")))
             column = convert_input_to_coordinate(int(input("Which column? (start with 1) ")))
-            available = check_space_empty(game, row)
+            available = check_space_empty(game, row, column) #3; add column to argument
         game = add_piece(game, player, row, column)
         display_game(game)
         player = switch_player(player)
-#        winner = check_winner(game)
+        winner = check_winner(game) #4; Uncomment the line
     display_winner(winner)
